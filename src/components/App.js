f9ff3ac45bf0a64bcc8cdb837f15558687ef6027
Erik <Erik.quintana@icloud.com>
2022-06-07 15:58:24 -0600

    about

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import blogData from "../data/blog";
 import Header from "./Header";
-
+import About from "./About";
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
       <Header name={blogData.name}/>
+      <About 
+      image={blogData.image}
+      about={blogData.about}/>
     </div>
   );
 }
